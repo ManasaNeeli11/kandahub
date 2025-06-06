602e9147e06362293a4b298ce93cf9a6899e4d62
Manasa Neeli <manasaneeli57@gmail.com>
2025-06-06 16:30:08 +0530

    Fix ALLOWED_HOSTS and update settings.py for deployment

--- kandahub/settings.py
+++ kandahub/settings.py
@@ -29,7 +29,8 @@ DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
 
 # Allowed hosts
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kandahub.onrender.com']
+
 if not DEBUG:
     ALLOWED_HOSTS.append('kandahub.onrender.com')
 
