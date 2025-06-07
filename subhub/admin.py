f262aba9001994be94080b531ac810d3bef97953
Manasa Neeli <manasaneeli57@gmail.com>
2025-06-07 15:31:14 +0530

    Configured quiz models in admin

--- subhub/admin.py
+++ subhub/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from .models import Quiz, Question, Choice, UserQuizResponse
 
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(UserQuizResponse)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'num_questions')
+    inlines = [QuestionInline]
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'quiz')
+    inlines = [ChoiceInline]
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
+
+@admin.register(UserQuizResponse)
+class UserQuizResponseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'quiz', 'score', 'submitted_at')
