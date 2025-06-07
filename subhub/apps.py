9da22a14e3a7339469c2d78e3ef7d6c314c3cc52
Manasa Neeli <manasaneeli57@gmail.com>
2025-06-07 13:53:01 +0530

    Removed superuser creation from apps.py to prevent DB errors

--- subhub/apps.py
+++ subhub/apps.py
@@ -1,16 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError, ProgrammingError  # <-- Import these!
 
 class SubhubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'subhub'
-
-    def ready(self):
-        User = get_user_model()
-        try:
-            if not User.objects.filter(username='adminmanasa').exists():
-                User.objects.create_superuser('adminmanasa', 'manasaneeli57@gmail.com', '22am1a3157')
-        except (OperationalError, ProgrammingError):
-            # Database might not be ready, so ignore errors here
-            pass
