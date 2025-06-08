5960c0666813ab58e6154d96b5c198c7f66a2080
Manasa Neeli <manasaneeli57@gmail.com>
2025-06-08 12:23:39 +0530

    ✅ Check before creating superuser to avoid duplicate error

--- create_admin_once.py
+++ create_admin_once.py
@@ -1,14 +1,12 @@
 from django.contrib.auth import get_user_model
-import django
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kandahub.settings')
-django.setup()
 
 User = get_user_model()
+username = "adminmanasa"
+password = "adminpassword123"
+email = "admin@example.com"
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("adminmanasa", "manasaneeli57@gmail.com", "22am1a3157")
-    print("Superuser created.")
+if not User.objects.filter(username=username).exists():
+    print(f"✅ Creating superuser: {username}")
+    User.objects.create_superuser(username=username, password=password, email=email)
 else:
-    print("Superuser already exists.")
+    print(f"⚠️ Superuser '{username}' already exists.")
